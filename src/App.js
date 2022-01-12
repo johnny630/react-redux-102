1132bcde0aabcc3ce2ecdad56812e26db4bbc346
johnny630 <bakolama@gmail.com>
2022-01-12 12:32:00 +0800

    sync to server sample where state changed each time.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
-import React from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch('URL', {
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
 
   return (
     <Layout>
