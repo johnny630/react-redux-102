0867fc7b498f5d6c06fa9584670b5d4545bce6fe
johnny630 <bakolama@gmail.com>
2022-01-14 23:48:43 +0800

    add showNotification in ui-slice reducer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
 import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
 
   useEffect(() => {
-    fetch('URL', {
-      method: 'PUT',
-      body: JSON.stringify(cart),
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNitification({
+          status: 'pending',
+          title: 'Sending...',
+          message: 'Sending cart data',
+        })
+      );
+      const response = await fetch('URL', {
+        method:
+          'https://react-db-fcd86-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json',
+        body: JSON.stringify(cart),
+      });
+
+      if (!response.ok) {
+        throw new Error('Sending data failed');
+      }
+
+      dispatch(
+        uiActions.showNitification({
+          status: 'success',
+          title: 'Success!',
+          message: 'Send cart data successfully',
+        })
+      );
+    };
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNitification({
+          statue: 'error',
+          title: 'Error!',
+          message: 'Send cart data failed',
+        })
+      );
     });
   }, [cart]);
 
--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -2,11 +2,18 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const uiSlice = createSlice({
   name: 'ui',
-  initialState: { cartIsVisible: false },
+  initialState: { cartIsVisible: false, notification: null },
   reducers: {
     toggle(state) {
       state.cartIsVisible = !state.cartIsVisible;
     },
+    showNotification(state, action) {
+      state.notification = {
+        status = action.status,
+        title = action.title,
+        message = action.message,
+      }
+    }
   },
 });
 
