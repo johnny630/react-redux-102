0a4d72151f4a69e6006eee8fae81b85d2149cd33
johnny630 <bakolama@gmail.com>
2022-01-25 00:02:16 +0800

    retrieve sendCartData and fetchCartData to store/cart-actions.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
-import { sendCartData } from './store/cart-slice';
+import { sendCartData, fetchCartData } from './store/cart-actions';
 
 let initial = true;
 
@@ -16,12 +16,15 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    dispatch(sendCartData(cart));
+    dispatch(fetchCartData());
+  }, [dispatch]);
 
+  useEffect(() => {
     if (initial) {
       initial = false;
       return;
     }
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
